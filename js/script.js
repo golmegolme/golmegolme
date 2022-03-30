8dd7039f3a28ce31600f09def08febadaba9e6c1
golmegolme <eugene@gmail.com>
2022-03-30 16:00:25 +0900

    file6

--- js/script.js
+++ js/script.js
@@ -20,3 +20,24 @@ $(window).scroll(function() {
     else
         $('.header-top').css('opacity','.8');
 });
+// webView.configuration.allowsInlineMediaPlayback = true
+// if  #available ( iOS 10.0, *) {
+//     webConfiguration.mediaTypesRequiringUserActionForPlayback = []
+// } 다른 {
+//     // 이전 버전에 대한 폴백
+//     webConfiguration.mediaPlaybackRequiresUserAction = 거짓
+// }
+
+// var player;
+// function onYouTubeIframeAPIReady(){
+//   player = new YT.Player('#player',{
+//     width:'100%',
+//     videoId:'https://www.youtube.com/embed/06Xp7tLk0oU/iframe_api',
+//     playerVars:{'autoplay':1,'playsinline':1},
+//     events:{ 'onReady':onPlayerReady }
+//   });
+// }
+// function onPlayerReady(e){
+//   e.target.mute();
+//   e.target.playVideo();
+// }
